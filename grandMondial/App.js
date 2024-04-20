7760190b97ef0e8c40e59b3960920547c6d1938c
Maksym-Tykhonenko <97916981+Maksym-Tykhonenko@users.noreply.github.com>
2024-04-20 10:02:57 +0300

    initial commit

---
+++ grandMondial/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Hello GrandMondial!!!</Text>
+    </View>
+  );
+};
+
+
+export default App;
